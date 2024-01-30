79396c4af4af6afc4614b7551d39df5f4cb17ae1
gjsgabriel <gjsgabriel83@gmail.com>
2024-01-30 13:13:58 -0300

    Category

--- src/main/java/com/gjscontatos/config/TestConfig.java
+++ src/main/java/com/gjscontatos/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.gjscontatos.entities.Category;
 import com.gjscontatos.entities.Order;
 import com.gjscontatos.entities.User;
 import com.gjscontatos.entities.enums.OrderStatus;
+import com.gjscontatos.repository.CategoryRepository;
 import com.gjscontatos.repository.OrderRepository;
 import com.gjscontatos.repository.UserRepository;
 
@@ -23,9 +25,18 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics"); 
+		Category cat2 = new Category(null, "Books"); 
+		Category cat3 = new Category(null, "Computers"); 
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
 		User u1 = new User(null, "Jaqueline Caobeli", "jaqueline@gmail.com", "988888888", "123456"); 
 		User u2 = new User(null, "Gabriel silva", "gabriel@gmail.com", "977777777", "123456"); 
---
+++ src/main/java/com/gjscontatos/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.gjscontatos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gjscontatos.entities.Category;
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
