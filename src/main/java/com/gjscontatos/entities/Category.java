5dcae074a4bbc93d16f554b45190daeec01858ee
gjsgabriel <gjsgabriel83@gmail.com>
2024-02-06 11:25:52 -0300

    Many-to-many association with joinTable

--- src/main/java/com/gjscontatos/entities/Category.java
+++ src/main/java/com/gjscontatos/entities/Category.java
@@ -1,12 +1,17 @@
 package com.gjscontatos.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,12 +25,14 @@ public class Category implements Serializable{
 	private Long id;
 	private String name; 
 	
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {	
 	}
 	
 	public Category(Long id, String name) {
-		super();
 		this.id = id;
 		this.name = name;
 	}
@@ -52,6 +59,10 @@ public class Category implements Serializable{
 		this.name = name;
 	}
 
+	public Set<Product> getProducts() {
+		return products;
+	}
+	
 
 	@Override
 	public int hashCode() {
@@ -70,6 +81,7 @@ public class Category implements Serializable{
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
+
 	
 	
 	
--- src/main/java/com/gjscontatos/entities/Product.java
+++ src/main/java/com/gjscontatos/entities/Product.java
@@ -1,12 +1,17 @@
 package com.gjscontatos.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +28,12 @@ public class Product implements Serializable{
 	private String description;
 	private Double price;
 	private String imgUrl;
+	
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", 
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
 	
 	public Product() {
 	}
@@ -75,6 +86,11 @@ public class Product implements Serializable{
 		this.imgUrl = imgUrl;
 	}
 
+	public Set<Category> getCategories() {
+		return categories;
+	}
+	
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -91,6 +107,7 @@ public class Product implements Serializable{
 		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
+
 	
 	
 	
